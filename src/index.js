8f73f63db52dbf2e83fc7f736599e2e4d7d79dcd
meistersolutions <meistersolutions88@gmail.com>
2024-11-11 21:19:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // index.js
 const express = require('express');
 const dotenv = require('dotenv');
-const { getClientID, getWelcomeMessage, getMainMenu } = require('./dbController');
+const { getClientID, getWelcomeMessage, getMainMenu, getFromList, getPocFromPoc, getAvailableDates, getAvailableTimes } = require('./dbController');
 const { connectDB } = require('./db');
 const {
     sendWhatsAppMessage,
-    sendInteractiveMessage
+    sendInteractiveMessage,
+    sendRadioButtonMessage
 } = require('./utils');
 
 dotenv.config();
@@ -36,6 +37,10 @@ app.get('/webhook', (req, res) => {
 
 app.post('/webhook', async(req, res) => {
     const body = req.body;
+
+   // console.log(`webhook received: ${JSON.stringify(body, null, 2)}`);
+    let title = [];
+    let Response_id = [];
 
     if (body.object) {
         const changes = body.entry && body.entry[0].changes && body.entry[0].changes[0].value.messages;
@@ -75,8 +80,63 @@ app.post('/webhook', async(req, res) => {
                     // Retrieve the first menu item's HEADER_MESSAGE (assuming only one HEADER_MESSAGE for the main menu)
                     const headerMessage = mainMenuItems[0].HEADER_MESSAGE;
                     // Extract MENU_NAME items for interactive message
-                    const menuNames = mainMenuItems.map(item => ({ id: item.MENU_NAME, title: item.MENU_NAME }));
+                    const menuNames = mainMenuItems.map(item => ({ id: item.CLIENT_ID+'-'+item.MENU_ID+'-'+item.MENU_ID, title: item.MENU_NAME }));
                     sendInteractiveMessage(from, headerMessage, menuNames);
+
+                } catch (error) {
+                    console.error("Error fetching main menu:", error);
+                    sendWhatsAppMessage(from, "Sorry, an error occurred while fetching the menu.");
+                }
+            }
+            else{
+                // Extract the title from the JSON data
+                message=body.entry[0].changes[0].value.messages[0];
+                if (message.interactive) {
+                    const interactiveType = message.interactive.type;
+                    if (interactiveType === 'button_reply' && message.interactive.button_reply) {
+                       title = message.interactive.button_reply.title;
+                       Response_id = message.interactive.button_reply.id.split('-'); 
+                    } else if (interactiveType === 'list_reply' && message.interactive.list_reply) {
+                       title = message.interactive.list_reply.title;
+                       Response_id = message.interactive.list_reply.id.split('-');
+                    }
+                }
+                
+                console.log(`Title: ${title} ID: ${Response_id}` );
+                
+                clientId = Response_id[0];
+                menuId = Response_id[1];
+                selectId= Response_id[2];
+
+                try {
+                    // Get the main menu for initial interaction
+                    const mainMenuItems = await getMainMenu(clientId, menuId);
+
+                    if (mainMenuItems.length === 0) {
+                        sendWhatsAppMessage(from, "No menu options available.");
+                        return;
+                    }
+                    else if (mainMenuItems.length === 1 && mainMenuItems[0].ACTION)
+                    {
+                        const actionMenuNames = await handleAction(mainMenuItems[0].ACTION.split(':'),clientId,mainMenuItems[0].MENU_ID,title,selectId)
+                       
+                        // Retrieve the first menu item's HEADER_MESSAGE (assuming only one HEADER_MESSAGE for the main menu)
+                        const headerMessage = mainMenuItems[0].HEADER_MESSAGE;
+                        
+                        // Extract MENU_NAME items for interactive message
+                        
+                        const menuNames = actionMenuNames.map(item => ({ id: item.CLIENT_ID+'-'+item.MENU_ID+'-'+item.ITEM_ID, title: item.MENU_NAME }));
+                        await sendRadioButtonMessage(from, headerMessage, menuNames);
+                    }
+                    else
+                    {
+
+                        // Retrieve the first menu item's HEADER_MESSAGE (assuming only one HEADER_MESSAGE for the main menu)
+                        const headerMessage = mainMenuItems[0].HEADER_MESSAGE;
+                        // Extract MENU_NAME items for interactive message
+                        const menuNames = mainMenuItems.map(item => ({ id: item.CLIENT_ID+'-'+item.MENU_ID+'-'+item.ITEM_ID, title: item.MENU_NAME }));
+                        await sendRadioButtonMessage(from, headerMessage, menuNames);
+                    }
 
                 } catch (error) {
                     console.error("Error fetching main menu:", error);
@@ -91,6 +151,40 @@ app.post('/webhook', async(req, res) => {
         res.sendStatus(404);
     }
 });
+
+async function handleAction(iAction,iClientId,iMenuId,iUserValue,iSelectId) {
+    const iLang='ENG';
+    console.log(`handleAction: iAction:${iAction} ,iClientID:${iClientId} ,iMenuId:${iMenuId}, iUserValue:${iUserValue} iSelectId: ${iSelectId}`);
+    console.log(`handleAction: iAction[0]:${iAction[0]}`);
+    // Action-based handling
+    if (iAction[0] === 'LIST') {
+        const oList = await getFromList(iClientId,iMenuId,iAction[1],iLang);   
+        return oList;     
+    }
+    else if (iAction[0] === 'POC')
+    {
+        const oList = await getPocFromPoc(iClientId,iMenuId,iUserValue);   
+        return oList;
+    }
+    else if(iAction[0] === 'FETCH_AVAILABLE_DATES_DIRECT')
+    {
+        console.log('Inside FETCH_AVAILABLE_DATES_DIRECT');
+        const oList = await getAvailableDates(iClientId,iMenuId,iSelectId);   
+        return oList;
+        
+    }
+    else if(iAction[0] === 'FETCH_AVAILABLE_TIMES_DIRECT')
+    {
+        console.log('Inside FETCH_AVAILABLE_TIMES_DIRECT');
+        const oList = await getAvailableTimes(iClientId,iMenuId,iSelectId,iUserValue);   
+        return oList;
+        
+    }
+    else
+    {
+        console.log('handleAction:inside Else');
+    }
+}
 
 // Start the server
 app.listen(port, () => {
